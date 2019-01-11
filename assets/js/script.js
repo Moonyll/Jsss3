bbcbe66f3546a8d318f6225f69c59fef58491026
Moonyll <s.pouwel@laposte.net>
2019-01-11 15:56:50 +0100

    Jsss3

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+﻿function gr()
+{
+document.getElementById('text').style.color = 'green';
+}
+function rd()
+{
+document.getElementById('text').style.color = 'red';
+}
+function bl()
+{
+document.getElementById('text').style.color = 'blue';
+}
